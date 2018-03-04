41509d4d6d6232ce50fd769b2127473dcf46b908
Dirceu Schlickmann <dschli2007@gmail.com>
2018-03-04 13:42:29 -0300

    feat: Implement component BookShelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
+import Book from './Book'
+import PropTypes from 'prop-types'
 
 class BookShelf extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    title: PropTypes.string.isRequired
+  }
+
   render() {
-    return <p>This is a shelf</p>
+    const { title, books } = this.props
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book) => (
+              <li>
+                <Book book={book} />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
   }
 }
 
