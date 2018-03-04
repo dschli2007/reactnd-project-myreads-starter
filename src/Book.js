87fe6288a153da7aa6ac6908c793213c50f36b67
Dirceu Schlickmann <dschli2007@gmail.com>
2018-03-04 13:32:28 -0300

    feat: Implement Book Component

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired
+  }
+
   render() {
+    const book = this.props.book
     return (
       <div className="book">
         <div className="book-top">
@@ -10,8 +16,7 @@ class Book extends Component {
             style={{
               width: 128,
               height: 188,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")'
+              backgroundImage: `url("${book.imageLinks.smallThumbnail}")`
             }}
           />
           <div className="book-shelf-changer">
@@ -26,8 +31,8 @@ class Book extends Component {
             </select>
           </div>
         </div>
-        <div className="book-title">Ender's Game</div>
-        <div className="book-authors">Orson Scott Card</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
       </div>
     )
   }
