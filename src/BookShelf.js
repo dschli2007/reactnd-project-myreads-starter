a2c5d788f8c9ec4930000b5e089eb6bb2d6eb770
Dirceu Schlickmann <dschli2007@gmail.com>
2018-03-04 12:19:42 -0300

    chore: Rename BookShelve to BookShelf

---
+++ src/BookShelf.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class BookShelf extends Component {
+  render() {
+    return <p>This is a shelf</p>
+  }
+}
+
+export default BookShelf
--- src/BookShelve.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class BookShelve extends Component {
-  render() {
-    return <p>This is a shelve</p>
-  }
-}
-
-export default BookShelve
